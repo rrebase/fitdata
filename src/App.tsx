4b470066e1971c30542eb352fb6fb2825311773b
Ragnar Rebase <rrebase@gmail.com>
2020-01-31 16:08:45 +0200

    Add icon.

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,18 @@ const App: React.FC = () => {
     <Provider store={store}>
       <div className="App">
         <div className="container">
+          <svg
+            width="56"
+            height="42"
+            viewBox="0 0 56 42"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M54.6875 35C55.4094 35 56 35.5906 56 36.3125V40.6875C56 41.4094 55.4094 42 54.6875 42H1.3125C0.590625 42 0 41.4094 0 40.6875V1.3125C0 0.590625 0.590625 0 1.3125 0H5.6875C6.40937 0 7 0.590625 7 1.3125V35H54.6875ZM40.7641 10.4453L31.5 16.625L22.1703 4.18906C21.6125 3.44531 20.475 3.5 19.9938 4.29844L10.5 20.125V31.5H52.5L42.6672 10.9594C42.3172 10.2484 41.4203 10.0078 40.7641 10.4453V10.4453Z"
+              fill="#E9AA52"
+            />
+          </svg>
           <h1 className="name">FitData</h1>
           <FileInput value={files} onChange={handleFileInputChange} />
           {error && <div className="error">{error}</div>}
